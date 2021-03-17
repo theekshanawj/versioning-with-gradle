6954f01be896a6b522d6669f8505ebb9c0bfd09f
theekshanawj <theekshanawjdev@gmail.com>
2021-03-17 13:26:38 +0530

    Gradle versioning

---
+++ src/test/java/com/theekshana/gradle/versioning/VersioningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theekshana.gradle.versioning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersioningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
